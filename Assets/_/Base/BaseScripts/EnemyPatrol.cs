8acbe3c56a2ae6ed159ae8ef77f3a39eb66decc8
GabrielBaf <68453589+GabrielBaf@users.noreply.github.com>
2022-08-05 01:24:01 -0700

    Squashed commit of the following:
    commit d659d6fa1b91e9eeee597ec74541a3ca78d9ea79
    Author: leoferreira23 <fsleonardo91@gmail.com>
    Date:   Thu Aug 4 16:46:32 2022 -0700
    
        enemy patrolling
    
        enemy patrolling
    
    commit 5f98a2f72ae46d8cd10246e350062445547937eb
    Author: leoferreira23 <fsleonardo91@gmail.com>
    Date:   Tue Aug 2 00:15:54 2022 -0700
    
        criado terreno
    
        criado terreno

---
+++ Assets/_/Base/BaseScripts/EnemyPatrol.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D rb;
+    BoxCollider2D cb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        cb = GetComponent<BoxCollider2D>();
+    }
+
+    void Update() {
+        if(isFacingDown()) {
+            rb.velocity = new Vector2(0f, -moveSpeed);
+        } else {
+            rb.velocity = new Vector2(0f, moveSpeed);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
+    }
+
+    private bool isFacingDown() {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+
+}
